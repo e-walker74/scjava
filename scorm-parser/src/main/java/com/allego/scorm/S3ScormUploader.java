219b9b848f192b3cb7cb83ae1b6e506f83f89356
akosmachev <alexey.kosmachev@gmail.com>
2017-05-31 23:04:18 +0300

    lambda function

--- scorm-parser/src/main/java/com/allego/scorm/S3ScormUploader.java
+++ scorm-parser/src/main/java/com/allego/scorm/S3ScormUploader.java
@@ -30,45 +30,45 @@ import java.util.List;
  */
 public class S3ScormUploader  implements IScormParser{
 
-    private static final int SCORM_1_2 = 1;
-    private static final int SCORM_2004 = 2;
+    private  static final int SCORM_1_2 = 1;
+    private  static final int SCORM_2004 = 2;
 
     private final String[] SCORM2004SchemaLocations = {"http://www.imsglobal.org/xsd/imscp_v1p1 imscp_v1p1.xsd",
-            "http://www.adlnet.org/xsd/adlcp_v1p3 adlcp_v1p3.xsd",
-            "http://www.adlnet.org/xsd/adlseq_v1p3 adlseq_v1p3.xsd",
-            "http://www.adlnet.org/xsd/adlnav_v1p3 adlnav_v1p3.xsd",
-            "http://www.imsglobal.org/xsd/imsss imsss_v1p0.xsd"};
+                                            "http://www.adlnet.org/xsd/adlcp_v1p3 adlcp_v1p3.xsd",
+                                            "http://www.adlnet.org/xsd/adlseq_v1p3 adlseq_v1p3.xsd",
+                                            "http://www.adlnet.org/xsd/adlnav_v1p3 adlnav_v1p3.xsd",
+                                            "http://www.imsglobal.org/xsd/imsss imsss_v1p0.xsd"};
 
     private final String[] SCORM12SchemaLocations = {"http://www.imsproject.org/xsd/imscp_rootv1p1p2 imscp_rootv1p1p2.xsd",
-            "http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd",
-            "http://www.adlnet.org/xsd/adlcp_rootv1p2 adlcp_rootv1p2.xsd"};
+                                            "http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd",
+                                            "http://www.adlnet.org/xsd/adlcp_rootv1p2 adlcp_rootv1p2.xsd"};
 
-    private final String SCORM12Signature = "1.2";
+    private final String SCORM12Signature ="1.2";
 
-    private final String SCORM2004Signature = "2004";
+    private final String SCORM2004Signature ="2004";
 
     private int detectSCORMVersion(Document manifestDocument) throws BadSCORMPackageException {
 
         NodeList list = manifestDocument.getElementsByTagName("schemaversion");
-        if (list.getLength() > 0) {
+        if (list.getLength()>0) {
             Node version = list.item(0);
-            String signature = version.getTextContent();
-            if (signature.length() > 0) {
-                if (signature.indexOf(SCORM12Signature) != -1) {
+            String signature  = version.getTextContent();
+            if (signature.length()>0) {
+                if (signature.indexOf(SCORM12Signature)!= -1) {
                     return SCORM_1_2;
-                } else if (signature.indexOf(SCORM2004Signature) != -1) {
+                } else if (signature.indexOf(SCORM2004Signature)!= -1) {
                     return SCORM_2004;
                 }
 
             }
         }
-        String locationsPlain = manifestDocument.getDocumentElement().getAttribute("schemaLocation");
+        String locationsPlain  = manifestDocument.getDocumentElement().getAttribute("schemaLocation");
         locationsPlain = locationsPlain.trim().replaceAll("\\s{2,}", " ");
 
-        String[] locations = locationsPlain.split("\\s");
+        String[] locations =locationsPlain.split("\\s");
         List<String> listSCORM2004 = Arrays.asList(SCORM2004SchemaLocations);
 
-        for (int i = 0; i < locations.length; i++) {
+        for (int i=0;i<locations.length;i++) {
             if (listSCORM2004.contains(locations[i])) {
                 return SCORM_2004;
             }
@@ -76,7 +76,7 @@ public class S3ScormUploader  implements IScormParser{
 
         List<String> listSCORM12 = Arrays.asList(SCORM12SchemaLocations);
 
-        for (int i = 0; i < locations.length; i++) {
+        for (int i=0;i<locations.length;i++) {
             if (listSCORM12.contains(locations[i])) {
                 return SCORM_1_2;
             }
@@ -96,15 +96,16 @@ public class S3ScormUploader  implements IScormParser{
             Document manifestDocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(manifestStream);
 
             String templateName = null;
-            int packageType = detectSCORMVersion(manifestDocument);
+             int packageType = detectSCORMVersion(manifestDocument);
+
 
 
             if (packageType == SCORM_1_2) {
-                templateName = "scorm12.xsl";
-                version = IScormParserResult.SCORM_1_2;
+                    templateName = "scorm12.xsl";
+                    version = IScormParserResult.SCORM_1_2;
 
             } else {
-                templateName = "scorm2004.xsl";
+                    templateName = "scorm2004.xsl";
                 version = IScormParserResult.SCORM_2004;
             }
 
@@ -118,31 +119,32 @@ public class S3ScormUploader  implements IScormParser{
             Source source = new StreamSource(new ByteArrayInputStream(manifest.getBytes()));
             ByteArrayOutputStream resultStream = new ByteArrayOutputStream();
 
-            Result resultDef = new StreamResult(resultStream);
+            Result resultDef =  new StreamResult(resultStream);
 
             xformer.transform(source, resultDef);
             String defSCORMData = resultStream.toString();
 
             JSONObject json = new JSONObject(defSCORMData);
 
-            result = new ScormPackage(version, json);
+            result =  new ScormPackage(version,json);
 
         } catch (Exception e) {
             throw new ParserConfigurationException(e.getMessage());
         }
 
 
+
+
         return result;
     }
-
     private void uploadToS3(String pathToInputScormPackage, String s3AccessKey, String s3SecretKey, String bucket, String parentTargetPath) throws DirectoryAlreadyExistsException, ScormUploadException {
         AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretKey);
         AmazonS3 s3client = new AmazonS3Client(credentials);
-        try {
-            S3Object folder = s3client.getObject(bucket, parentTargetPath);
+        try{
+            S3Object folder =  s3client.getObject(bucket,parentTargetPath);
 
             throw new DirectoryAlreadyExistsException("S3 directory already exists");
-        } catch (AmazonServiceException e) {
+        }catch (AmazonServiceException e) {
             String errorCode = e.getErrorCode();
             if (!errorCode.equals("NoSuchKey")) {
                 throw e;
@@ -152,18 +154,24 @@ public class S3ScormUploader  implements IScormParser{
         try {
 
             File file = new File(pathToInputScormPackage);
+            String filePath;
+            if (parentTargetPath.endsWith("/")) {
+                filePath = parentTargetPath+"scorm.zip";
+            } else {
+                filePath = parentTargetPath+"/scorm.zip";
+            }
             s3client.putObject(new PutObjectRequest(
-                    bucket, parentTargetPath, file));
+                    bucket, filePath, file));
 
         } catch (AmazonServiceException ase) {
-            throw new ScormUploadException(ase.getErrorMessage());
+           throw new ScormUploadException(ase.getErrorMessage());
         } catch (AmazonClientException ace) {
             throw new ScormUploadException(ace.getMessage());
         }
 
     }
 
-    public IScormParserResult parse(String pathToInputScormPackage, String s3AccessKey, String s3SecretKey, String bucket, String parentTargetPath) {
+    public IScormParserResult parse(String pathToInputScormPackage, String s3AccessKey, String s3SecretKey, String bucket, String parentTargetPath)  {
 
         UnZip zip = new UnZip(pathToInputScormPackage);
         int parserResult = IScormParserResult.STATUS_SUCCESS;
@@ -193,10 +201,10 @@ public class S3ScormUploader  implements IScormParser{
             uploadToS3(pathToInputScormPackage, s3AccessKey, s3SecretKey, bucket, parentTargetPath);
         } catch (DirectoryAlreadyExistsException e) {
             parserResult = IScormParserResult.STATUS_AWS_S3_EXCEPTION;
-            packageSCORM = null;
+            packageSCORM =  null;
         } catch (ScormUploadException e) {
             parserResult = IScormParserResult.STATUS_AWS_S3_EXCEPTION;
-            packageSCORM = null;
+            packageSCORM =  null;
         }
 
         S3CloudResult result;
---
+++ scorm-parser/target/classes/com/allego/scorm/scorm12.xsl
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:imscp="http://www.imsproject.org/xsd/imscp_rootv1p1p2"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                version="2.0"
+                xsi:schemaLocation="http://www.imsproject.org/xsd/imscp_rootv1p1p2 imscp_rootv1p1p2.xsd
+                           http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd"
+
+
+>
+
+    <xsl:output indent="no" omit-xml-declaration="yes" method="text" encoding="utf-8"/>
+    <xsl:strip-space elements="*"/>
+    <xsl:key name="resourceUrl" match="//imscp:resource" use="@identifier"/>
+
+    <xsl:template match="/">
+        <xsl:text>{</xsl:text>
+
+        <xsl:apply-templates select="imscp:manifest/imscp:organizations"/>
+
+
+        <xsl:text>}</xsl:text>
+    </xsl:template>
+    <xsl:template match="imscp:organizations">
+        <xsl:if test="@default !=''">
+            &quot;default&quot;:&quot;<xsl:value-of select="@default"/>&quot;,
+        </xsl:if>
+        <xsl:text>&quot;bundles&quot;:[</xsl:text>
+        <xsl:apply-templates select="imscp:organization"/>
+        <xsl:text>]</xsl:text>
+    </xsl:template>
+    <xsl:template match="imscp:organization">
+        <xsl:text>{</xsl:text>
+        <xsl:text>&quot;id&quot;:</xsl:text>&quot;<xsl:value-of select="@identifier"/>&quot;,
+        <xsl:text>&quot;title&quot;:</xsl:text>&quot;<xsl:value-of select="imscp:title"/>&quot;,
+        <xsl:text>&quot;items&quot;:[</xsl:text>
+        <xsl:apply-templates select="imscp:item"/>
+        <xsl:text>]</xsl:text>
+        <xsl:text>}</xsl:text>
+        <xsl:if test="position() != last()">
+            <xsl:text>,</xsl:text>
+        </xsl:if>
+    </xsl:template>
+    <xsl:template match="imscp:item">
+        <xsl:text>{</xsl:text>
+        <xsl:text>&quot;id&quot;:</xsl:text>&quot;<xsl:value-of select="@identifier"/>&quot;
+        <xsl:if test="@identifierref !=''">
+            <xsl:text>,&quot;url&quot;:</xsl:text>&quot;<xsl:value-of select="key('resourceUrl',@identifierref)/@href"/>&quot;
+        </xsl:if>
+
+        <xsl:if test="@isvisible !=''">
+            <xsl:text>,&quot;isVisible&quot;:</xsl:text>&quot;<xsl:value-of select="@isvisible"/>&quot;
+        </xsl:if>
+
+
+        <xsl:text>,&quot;title&quot;:</xsl:text>&quot;<xsl:value-of select="imscp:title"/>&quot;
+
+        <xsl:if test="imscp:item !=''">
+            <xsl:text>,&quot;items&quot;:[</xsl:text>
+            <xsl:apply-templates select="imscp:item"/>
+            <xsl:text>]</xsl:text>
+        </xsl:if>
+        <xsl:text>}</xsl:text>
+        <xsl:if test="position() != last()">
+            <xsl:text>,</xsl:text>
+        </xsl:if>
+    </xsl:template>
+    <xsl:template match="text()"/>
+
+</xsl:stylesheet>
